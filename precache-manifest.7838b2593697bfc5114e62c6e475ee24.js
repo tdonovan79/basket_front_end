e489a6a4458def78c5aa130ceff45ebb65eceaf6
tdonovan79 <tdonovan79@gmail.com>
2020-04-07 17:32:02 -0400

    Updates

---
+++ precache-manifest.7838b2593697bfc5114e62c6e475ee24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ee761b2be4601f04d766cf3862835e",
+    "url": "/basket_front_end/index.html"
+  },
+  {
+    "revision": "06a8b18da5f312ddede8",
+    "url": "/basket_front_end/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "dcf0b5e96c5aba62721f",
+    "url": "/basket_front_end/static/js/2.84240fe6.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/basket_front_end/static/js/2.84240fe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a8b18da5f312ddede8",
+    "url": "/basket_front_end/static/js/main.94d5843a.chunk.js"
+  },
+  {
+    "revision": "f15af98de25673bdf36a",
+    "url": "/basket_front_end/static/js/runtime-main.6b9ab2bd.js"
+  }
+]);
